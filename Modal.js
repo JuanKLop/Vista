e0d6f07fb4af6bb57c7b60c319cc33d9d6eebb4e
JM4P1R0 <juanmanuelpinor@gmail.com>
2024-09-23 12:21:35 -0500

    primer comit

---
+++ Modal.js
@@ -0,0 +1,34 @@
+// Cerrar el modal al presionar "Cancelar"
+var cancelButton = document.querySelector(".next-room2");
+if (cancelButton) {
+    cancelButton.addEventListener("click", function() {
+        addRoomModal.style.display = 'none';  // Cierra el modal de la habitación
+    });
+}
+// Función para abrir/cerrar el sidebar
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const main = document.querySelector('main');
+    sidebar.classList.toggle('active');
+    main.classList.toggle('shifted');
+}
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('main .content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex"; // Cambiar a "flex" para centrar el modal
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+
+
+
+
